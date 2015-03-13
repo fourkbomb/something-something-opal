9050caf6a3f32bdd282ffe6e2b139c24ef9f4916
keepcalm444 <keepcalm444@gmail.com>
2015-03-14 10:55:01 +1100

    Add dump.sh to dump stop info to a postgresql database

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # something-something-opal
 
 1. `pip3 install -r requirements.txt`
-2. profit
+2. configure psql
+2. download full gtfs data from tdx.transportnsw.info
+3. extract stops.txt and put in db folder
+4. run dump.sh
+5. profit
---
+++ db/dump.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+DBNAME=gtfs
+echo Creating $DBNAME db...
+createdb $DBNAME 2>/dev/null
+echo Done
+
+echo Initialising tables
+psql $DBNAME -c "CREATE TABLE stops (id text, name text, lat decimal, long decimal, parent_station text, wheelchair_boarding numeric, platform_code text)" 2>/dev/null
+echo Done
+
+echo Dumping CSV file
+#cat stops.txt | awk -F dump_stops.awk | pgsql gtfs
+#psql gtfs -c  \
+SED="s/'\"/'/g"
+TOTAL=`cat stops.txt | wc -l`
+COMPLETED=0
+DATA=`cat stops.txt | sed 's/ /\\\@/g'`
+for i in $DATA; do
+	statement="`echo $i | sed 's/\\\@/ /g' | awk -f dump_stops.awk`"
+	psql gtfs -c "$statement" >/dev/null
+	printf "$((TOTAL--)) remaining\r"
+	
+done
