1f4b25742029685160e6f2e041c100130b9abfaa
Harrison <harrison@theshoebridges.com>
2015-03-13 12:50:34 +1100

    app: refractor application -> app due to _standards_

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, World!")
 
-application = tornado.web.Application([
+app= tornado.web.Application([
     (r"/", MainHandler)
 ])
 
 if __name__ == "__main__":
-    application.listen(8080)
+    app.listen(8080)
     tornado.ioloop.IOLoop.instance().start()
 
 # vim: set ts=4 et
