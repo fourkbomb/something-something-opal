720d80779b93885f61edfc57e7157181a09594f5
keepcalm444 <keepcalm444@gmail.com>
2015-03-14 20:49:03 +1100

    Index: 'pages'. Zepto and velocity.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,53 @@
-console.log("Hello, World!");
+
+//console.log("Hello, World!");
+
+// 1. select mode
+var mode;
+var order = ['initial', 'pick-journey'];
+function modeChosen(ev) {
+	//window.lastEvent = ev;
+	mode = ev.target.id.split('-')[2];
+	//window.location.hash = '#pick-journey';
+	scrollTo('pick-journey');
+}
+
+
+// navigation
+var currentlyVisible = '#screen-initial';
+function scrollTo(id) {
+	//$(id).velocity('scroll');
+	//console.log(id);
+	var selector='#screen-'+id;
+	if (id != 'initial') {
+		selector += ',#btn-back';
+	} else {
+		currentlyVisible += ',#btn-back';
+	}
+	$(currentlyVisible).velocity('slideUp');
+	$(selector).velocity('slideDown');
+	
+	
+	currentlyVisible = '#screen-'+id;
+}
+
+function back() {
+	var current = order.indexOf(currentlyVisible.replace('#screen-',''));
+	scrollTo(order[current-1]);
+}
+
+function testScroll() {
+	$("html").velocity("scroll", { offset: "750px", mobileHA: false });
+}
+
+// misc DOM functions
+function domReady() {
+	if (document.readyState !== 'complete') return;
+
+	$('.btn-mode-select').on('click', modeChosen);
+	$('#btn-back').on('click', back);
+	//$(document).on('hashChange', hashChange);
+	console.log('Done');
+}
+
+document.addEventListener('readystatechange', domReady);
+
